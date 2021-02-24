aa5ac22c98b15f2d14f0b61c5669ce076643619d
matthewthomasgb <matthewthomasgb@gmail.com>
2021-02-24 20:17:56 +0000

    chore(refactor): amend stack name
    amend stack name from inherited template to K6AwsCloudwatchCdkStack

--- bin/k6-aws-cloudwatch-cdk.ts
+++ bin/k6-aws-cloudwatch-cdk.ts
@@ -4,4 +4,4 @@ import * as cdk from '@aws-cdk/core';
 import { K6AwsCloudwatchCdkStack } from '../lib/k6-aws-cloudwatch-cdk';
 
 const app = new cdk.App();
-new K6AwsCloudwatchCdkStack(app, 'AwsCdkTemplateStack');
+new K6AwsCloudwatchCdkStack(app, 'K6AwsCloudwatchCdkStack');
